2dc6e8c30fb53b034f1a702cc0ac598a9c4a04dd
Jan Bocinec <jan.bocinec@webikon.sk>
2020-03-26 21:23:50 +0100

    first commit

---
+++ src/components/CoreWidget.js
@@ -0,0 +1,169 @@
+import React from 'react'
+
+import { List } from 'part:@sanity/components/lists/default'
+import Spinner from 'part:@sanity/components/loading/spinner'
+import Button from 'part:@sanity/components/buttons/default'
+
+import styles from './Now.css'
+
+import CurrentDeployment from './CurrentDeployment'
+import Deployments from './Deployments'
+
+class CoreWidget extends React.Component {
+  _interval = undefined
+
+  constructor(props) {
+    super(props)
+    this.config = props.config
+  }
+
+  state = {
+    deploymentStatus: null,
+    deployments: null,
+    deploying: null,
+    hasDeployments: false,
+    error: null,
+  }
+
+  deployNow = () => {
+    const requestOptions = {
+      method: 'POST',
+    }
+    fetch(this.config.deployHook, requestOptions)
+      .then(async (response) => {
+        const data = await response.json()
+
+        // // check for error response
+        // if (!response.ok) {
+        //     // get error message from body or default to response status
+        //     const error = (data && data.message) || response.status;
+        //     return Promise.reject(error);
+        // }
+
+        this.setState({ deploymentStatus: data.job })
+      })
+      .catch((error) => {
+        this.setState({ error: error })
+        console.error('There was an error!', error)
+      })
+      .then(() => {
+        this.setState({ deploying: true })
+        this.checkDeployments()
+      })
+  }
+
+  getDeployment = () => {
+    this.getDeployments()
+  }
+
+  getDeployments = () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.config.token,
+      },
+    }
+    // https://badgen.net/
+    // https://iconify.design/icon-sets/?query=zeit
+
+    fetch(
+      'https://api.zeit.co/v5/now/deployments?teamId=' +
+        this.config.teamId +
+        '&projectId=' +
+        this.config.projectId +
+        '&limit=5',
+      requestOptions
+    )
+      .then(async (response) => {
+        const data = await response.json()
+        // // check for error response
+        // if (!response.ok) {
+        //     // get error message from body or default to response status
+        //     const error = (data && data.message) || response.status;
+        //     return Promise.reject(error);
+        // }
+        this.setState({ deployments: data.deployments })
+      })
+      .catch((error) => {
+        this.setState({ error: error })
+        console.error('There was an error!', error)
+      })
+      .then(() => {
+        this.isDeploying()
+      })
+  }
+
+  checkDeployments = () => {
+    if (typeof this._interval === 'undefined') {
+      this._interval = setInterval(this.getDeployments, 60000)
+    }
+  }
+
+  isDeploying = () => {
+    let currentState = false
+    this.state.deployments.some((deployment) => {
+      if (deployment.state === 'QUEUED' || deployment.state === 'BUILDING') {
+        currentState = true
+        return true
+      }
+    })
+
+    if (this.state.deploymentStatus && currentState) {
+      this.setState({ deploymentStatus: null })
+    }
+
+    if (currentState || this.state.deploymentStatus) {
+      this.setState({ deploying: true })
+      this.checkDeployments()
+    } else {
+      this.setState({ deploying: false })
+      this._interval = clearInterval(this._interval)
+    }
+  }
+
+  componentDidMount() {
+    this.getDeployments()
+  }
+
+  render() {
+    const { deploymentStatus, deployments, deploying, error } = this.state
+    const isLoading = !deployments
+    const isDeploying = deploying
+
+    if (error) {
+      return <pre>{JSON.stringify(error, null, 2)}</pre>
+    }
+
+    return (
+      <>
+        {isLoading && (
+          <List className={styles.list}>
+            <Spinner center message="Loading items…" />
+          </List>
+        )}
+
+        {!isLoading && (
+          <List className={styles.list}>
+            <CurrentDeployment deployment={this.state.deploymentStatus} />
+            <Deployments deployments={this.state.deployments} />
+          </List>
+        )}
+
+        <div className={styles.footer}>
+          {(isDeploying && (
+            <Button bleed color="primary" kind="simple" onClick={this.getDeployment}>
+              Refresh
+            </Button>
+          )) || (
+            <Button bleed color="primary" kind="simple" onClick={this.deployNow}>
+              Build & deploy
+            </Button>
+          )}
+        </div>
+      </>
+    )
+  }
+}
+
+export default CoreWidget
